810a3153cf7b683dad880f43449c35cf30df83a2
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-17 23:02:39 +0300

    generated user class and added the constructor

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,30 +8,17 @@ import { DataService } from '../data.service';
 })
 export class SearchComponent implements OnInit {
 
-  public username: string | undefined;
-  public githubUserQuery: string | undefined;
-  public githubProfile: any;
-  public githubRepos: any;
+private username: string | undefined;  
   
 
   constructor(private dataService: DataService) { }
 
-  public findProfile(){
-    // this.dataService.updateProfile();
-    // this.
-  }
-
-  public searchUser(){
-    this.dataService.getProfile(this.username).subscribe((data) =>{
-      this.githubProfile = data;
-    });
-
-    this.dataService.getRepos(this.username).subscribe((data)=> {
-      this.githubRepos
-    })
-  }
-
+ 
   ngOnInit(): void {
+  }
+
+  findUser() {
+
   }
 
 }
