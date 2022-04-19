cbb6eb3960e96835e29685e913e4931d604697d1
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-19 22:31:10 +0300

    Added routing to the 404 error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
-  { path: "", component: HomeComponent }
+  { path: "", component: HomeComponent },
+  { path: "**", component: NotFoundComponent}
 ];
 
 @NgModule({
